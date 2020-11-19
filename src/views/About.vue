1e3c2475da68488ca3868c4ae8054f0e686612c2
PigeonsHouse <bullbull1230@gmail.com>
2020-11-19 17:05:53 +0000

    [INIT] 初めてのプッシュ

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="todo">
+    <h1>ここには課題などのTodoリストが設置されるよ。KIT。</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <p>ここにKIT時間割が表示される…はず。</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
